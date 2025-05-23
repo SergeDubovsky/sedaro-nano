7e5d99acab6eac0e8dcd8e199b042a7f24d42b21
Sergei Dubovsky <sergei.dubovsky@hii-tsd.com>
2025-05-23 09:33:10 -0400

    Add Terraform configurations for GitHub Actions secrets management, including variables and provider setup

---
+++ terraform-github-secrets/variables.tf
@@ -0,0 +1,9 @@
+variable "github_owner" {
+  type        = string
+  description = "The GitHub organization or user that owns the repository."
+}
+
+variable "repository_name" {
+  type        = string
+  description = "The name of the GitHub repository."
+}
