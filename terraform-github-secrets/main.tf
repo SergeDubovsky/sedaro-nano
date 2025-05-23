03c1ab16d52d0d5027487d8b5de02fa6e5afad0d
Sergei Dubovsky <sergei.dubovsky@hii-tsd.com>
2025-05-23 09:34:57 -0400

    Update GitHub Actions secret to use correct output variable for AWS role ARN

--- terraform-github-secrets/main.tf
+++ terraform-github-secrets/main.tf
@@ -1,5 +1,5 @@
 resource "github_actions_secret" "aws_role_arn" {
   repository      = var.repository_name
   secret_name     = "AWS_ROLE_ARN"
-  plaintext_value = data.terraform_remote_state.bootstrap.outputs.aws_iam_role_arn
+  plaintext_value = data.terraform_remote_state.bootstrap.outputs.github_actions_role_arn
 }
