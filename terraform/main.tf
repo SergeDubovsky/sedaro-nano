81a763e10e9c95b8131b5ab263de26302355b103
Sergei Dubovsky <sergei.dubovsky@hii-tsd.com>
2025-05-23 11:53:34 -0400

    Update EKS cluster version to 1.32 in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -57,7 +57,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = local.name
-  cluster_version = "1.27"
+  cluster_version = "1.32"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
